fcef67e8d7a38c0919df70fee61a573f706e28da
Tom_Delfosse <Tom-Delfosse@users.noreply.github.com>
2021-06-16 13:28:36 +0200

    put privateMsg into external js file

--- privateMsg.js
+++ privateMsg.js
@@ -1,6 +1,56 @@
-module.exports(triggerText, replyText);
-client.on('message', message => {
-  if (message.content.toLowerCase() === triggerText.toLowerCase()){
-    message.author.send(replyText)
-  }
-})
+module.exports = (client, config, replyText) => {
+  client.on('message', message => {
+    if (message.channel.type === 'dm' && !message.author.bot){
+      const currentDate = new Date();
+      let minutes = '00'
+      if (currentDate.getMinutes() < 10){
+        minutes = '0' + currentDate.getMinutes()
+      } else {
+        minutes = currentDate.getMinutes()
+      }
+
+      client.channels.cache.get(config.ADMIN_CHAN).send(
+        '\n« ' + 
+        message.content + 
+        ' »\n\nRédigé par **' + message.author.username + 
+        '**, le ' + days[currentDate.getDay()] + 
+        ' ' + 
+        currentDate.getDate() + 
+        ' ' + 
+        months[currentDate.getMonth()] +
+        ' ' + currentDate.getFullYear() +
+        ' à ' + currentDate.getHours() +
+        ':' + 
+        minutes +
+        '.\n__')
+
+      message.author.send(replyText).catch(console.error)
+    }
+  })
+
+  
+const days = [
+  'lundi',
+  'mardi',
+  'mercredi',
+  'jeudi',
+  'vendredi',
+  'samedi',
+  'dimanche',
+]
+
+const months = [
+  'janvier',
+  'février',
+  'mars',
+  'avril',
+  'mai',
+  'juin',
+  'juillet',
+  'août',
+  'septembre',
+  'octobre',
+  'novembre',
+  'décembre',
+]
+}

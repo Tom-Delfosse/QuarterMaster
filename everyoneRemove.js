5cd21bb8c688cdccf94fa8e926a830bfbdfd9a6e
Tom_Delfosse <tomdelfosse2000@gmail.com>
2022-01-18 21:00:14 +0100

    updated channel

--- everyoneRemove.js
+++ everyoneRemove.js
@@ -4,7 +4,7 @@ module.exports = (client) => {
     if (
       !message.author.bot && 
       message.content.match("@everyone") &&
-      message.channel.guild.id == 690937944764186674
+      message.channel.guild.id == 409102794381983744
       ) {
         message.author.send(`Il est interdit d'utiliser le @/everyone sur le serveur. Merci de ta compréhension matelot !`)
         message.delete()
